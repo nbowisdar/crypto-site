4bbce1b1161c6e288e7d0743db376ffe09ae6189
Kink <132131@gmail.com>
2022-06-01 10:09:19 +0300

    improved

--- crypto/models.py
+++ crypto/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,4 +13,5 @@ class Post(models.Model):
     with_deposit = models.BooleanField(default=True)
     def __str__(self):
         return self.title
-
+    def get_absolute_url(self):
+       return reverse('post_detail', args=[str(self.id)])
