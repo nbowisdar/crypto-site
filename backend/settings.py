bd7e69c3e69ab0fcccf42ef2eabfd69b05d5f143
Kink <132131@gmail.com>
2022-06-02 14:29:13 +0300

    addcss

--- backend/settings.py
+++ backend/settings.py
@@ -89,13 +89,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
